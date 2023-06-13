de8e4e4e3dd77e53cac7b96092b27ceae721dc33
paolojoaquinp <paolojoaquinpintoperez@gmail.com>
2023-06-13 10:35:09 -0400

    update config v1.0

--- models/server.js
+++ models/server.js
@@ -26,6 +26,7 @@ class Server {
     }
 
     middlewares() {
+        
         // Desplegar el directorio público
         this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
 
